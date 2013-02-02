593234647076550a1bcb8858af06bcd10c422708
Rainbow <Rainbow@Rainbow-PC>
2013-02-02 13:09:42 +0800

    Test add view port

---
+++ app/Application.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+Ext.application({
+    name: 'Duyun',
+    autoCreateViewport: true,
+    launch: function() {
+        // This is fired as soon as the page is ready
+    }
+});
---
+++ app/view/Viewport.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+Ext.define('Duyun.view.Viewport', {
+    extend: 'Ext.container.Viewport',
+    layout: 'fit',
+    items: [
+        {
+            title: 'Hello Ext',
+            html : 'Hello! Welcome to Ext JS.'
+        }
+    ]
+});
